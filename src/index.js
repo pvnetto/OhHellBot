d742424950e7d5ee07b6a64747e1ec5c2ab3495c
Paiva <pvnetto1@gmail.com>
2019-11-20 19:28:39 -0300

    remove webhook

--- src/index.js
+++ src/index.js
@@ -10,14 +10,15 @@ const CardStickerManager = require('./game/cards/card-sticker-manager');
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;  // Receives a port from heroku
-const expressApp = express()
+// const expressApp = express()
 
 // Setting up the stage
 const stage = new Stage([greeter, lobby, game, draw, bets, round], { default: 'greeter' });
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-expressApp.use(bot.webhookCallback(`/${process.env.BOT_SECRET_PATH}`));
-bot.telegram.setWebhook(`${process.env.BOT_URL}:8443/${process.env.BOT_SECRET_PATH}`);
+bot.telegram.deleteWebhook();
+// expressApp.use(bot.webhookCallback(`/${process.env.BOT_SECRET_PATH}`));
+// bot.telegram.setWebhook(`${process.env.BOT_URL}:8443/${process.env.BOT_SECRET_PATH}`);
 
 //get username for group command handling
 bot.telegram.getMe().then((botInfo) => {
@@ -56,19 +57,19 @@ bot.on('inline_query', async (ctx) => {
     return await ctx.answerInlineQuery(queryOptions, { cache_time: 0 });
 });
 
-expressApp.listen(port, async () => {
-    console.log(`Listening on port ${port}!`);
-});
+// expressApp.listen(port, async () => {
+//     console.log(`Listening on port ${port}!`);
+// });
 
-expressApp.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+// expressApp.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
 
-expressApp.post('/', (req, res) => {
-    console.log(req.body);
-    console.log("got it");
-    res.status(200).send();
-});
+// expressApp.post('/', (req, res) => {
+//     console.log(req.body);
+//     console.log("got it");
+//     res.status(200).send();
+// });
 
 
 bot.launch();
