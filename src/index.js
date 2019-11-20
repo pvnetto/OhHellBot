9224d986d12a3396493ff24d1e5e8e56eb0b723f
Paiva <pvnetto1@gmail.com>
2019-11-20 12:29:48 -0300

    fix webhook

--- src/index.js
+++ src/index.js
@@ -9,20 +9,15 @@ const CardStickerManager = require('./game/cards/card-sticker-manager');
 
 require('dotenv').config();
 
-const port = process.env.PORT || 8443 // Correct port will be returned here
-
+const port = process.env.PORT || 3000;  // Receives a port from heroku
 const expressApp = express()
 
 // Setting up the stage
 const stage = new Stage([greeter, lobby, game, draw, bets, round], { default: 'greeter' });
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-expressApp.use(bot.webhookCallback(`${process.env.BOT_SECRET_PATH}`));
-bot.telegram.setWebhook(`${process.env.BOT_URL}/${process.env.BOT_SECRET_PATH}`);
-
-expressApp.listen(port, () => {
-    console.log(`Listening on port ${port}!`);
-});
+expressApp.use(bot.webhookCallback(`/${process.env.BOT_SECRET_PATH}`));
+bot.telegram.setWebhook(`${process.env.BOT_URL}:8443/${process.env.BOT_SECRET_PATH}`);
 
 //get username for group command handling
 bot.telegram.getMe().then((botInfo) => {
@@ -51,12 +46,6 @@ bot.command('bets', (ctx) => {
     }
 });
 
-bot.command('check', (ctx) => {
-    console.log(`Showing stats for ${ctx.from.first_name}:`);
-    console.log(ctx.db);
-    console.log(ctx.session);
-});
-
 bot.on('inline_query', async (ctx) => {
     let queryOptions = [{ type: 'article', id: 0, title: `You're not playing Fodinha`, description: '', thumb_url: '', input_message_content: { message_text: 'Ei bora fodinha' } }];
     const userDb = ctx.db[ctx.from.id];
@@ -66,5 +55,20 @@ bot.on('inline_query', async (ctx) => {
 
     return await ctx.answerInlineQuery(queryOptions, { cache_time: 0 });
 });
+
+expressApp.listen(port, async () => {
+    console.log(`Listening on port ${port}!`);
+});
+
+expressApp.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+expressApp.post('/', (req, res) => {
+    console.log(req.body);
+    console.log("got it");
+    res.status(200).send();
+});
+
 
 bot.launch();
